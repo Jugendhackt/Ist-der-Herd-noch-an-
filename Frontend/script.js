c16d9a56493ac37a16b44cab97345a837ecd18d5
Benjamin Lukas Glock <glockbenjaminlukas@gmail.com>
2018-11-24 15:21:16 +0100

    bla

---
+++ Frontend/script.js
@@ -0,0 +1,11 @@
+  var an = true;
+function toggle(){
+  an = !an;
+  if(an){
+  document.getElementById("anzeige").innerHTML ="der Herd ist an";
+  }
+  else {
+    document.getElementById("anzeige").innerHTML ="der Herd ist aus";
+  }
+
+}
