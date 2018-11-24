7b7ee5836bff593ce58d4d6d97646c4371b08554
artcatu <artcatu@gmail.com>
2018-11-24 17:49:51 +0100

    Der Sensor kann jezt PostAPIs senden

--- Hardware/sensor/sensor.ino
+++ Hardware/sensor/sensor.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include "auth.h"
+#include "ESP8266HTTPClient.h"
+int id = 5;
+
 
 //const char* mqtt_server = "mqtt.stg.freifunk-iot.de";
 
@@ -44,12 +47,38 @@ void loop() {
   state = digitalRead(sensorpin);
   Serial.println(state);
   delay(2000);
-  if (WiFi.status() != WL_CONNECTED)
-  {
-      Serial.println("Hilfe, ich hab kein Internet, aaaah scheisse");
-    }
-   else {
-      Serial.println("Juhuuu Neuland, oh wow");
-    }
+  state = !state;
+
+    if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+ 
+       HTTPClient http;    //Declare object of class HTTPClient
+     
+       http.begin("http://http.stg.freifunk-iot.de/data/" + String(id));      //Specify request destination
+       http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+     
+       int httpCode = http.POST(String(state));   //Send the request
+       String payload = http.getString();                  //Get the response payload
+     
+       Serial.println(httpCode);   //Print HTTP return code
+       Serial.println(payload);    //Print request response payload
+
+       http.end();
+
+       http.begin("http://http.stg.freifunk-iot.de/data/" + String(id) + "?state=" + String(state));      //Specify request destination
+       http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+     
+       int httpCode2 = http.POST(String(state));   //Send the request
+       String payload2 = http.getString();                  //Get the response payload
+     
+       Serial.println(httpCode2);   //Print HTTP return code
+       Serial.println(payload2);    //Print request response payload
+     
+       http.end();  //Close connection
+     
+     }else{
+     
+        Serial.println("Error in WiFi connection");   
+     
+     }
 
 }
