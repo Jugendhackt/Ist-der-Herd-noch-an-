442b1d3b9a6f298b986a43210cfcf37a8c634364
artcatu <artcatu@gmail.com>
2018-11-24 14:38:33 +0100

    Print every line

--- Hardware/sensor/sensor.ino
+++ Hardware/sensor/sensor.ino
@@ -1,9 +1,18 @@
+int sensorpin = D3;
+boolean state = false;
 void setup() {
+  pinMode(sensorpin,INPUT);
+  Serial.begin(115200);
+  
   // put your setup code here, to run once:
 
 }
 
 void loop() {
+  state = digitalRead(sensorpin);
+  Serial.println(state);
+  delay(2000);
+  
   // put your main code here, to run repeatedly:
 
 }
